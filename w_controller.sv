e8e13a174999a8bb05495b88d45466142ce651fa
Rehan Qasim <rehanbhatti0317@gmail.com>
2023-11-22 17:53:15 +0500

    removed duplication of ns state

--- w_controller.sv
+++ w_controller.sv
@@ -4,25 +4,24 @@ module w_controller (
     start,
     if_ready,
     w_done,
-    output logic start_late_if,
+    output logic start_if,
     w_read,
     switch,
     clr_w,
     ready
 );
-  logic start_if;
-  always_ff @(posedge clk) begin : blockName
-    if (rst) start_late_if <= 0;
-    else start_late_if <= start_if;
-  end
+  // logic start_if;
+  // always_ff @(posedge clk) begin : blockName
+  //   if (rst) start_late_if <= 0;
+  //   else start_late_if <= start_if;
+  // end
   localparam READY = 2'b00;
   localparam FETCH = 2'b01;
   localparam WAIT = 2'b10;
   logic [1:0] ns, cs;
-  always @(posedge clk) begin
+  always_comb begin
     if (cs == READY) begin
       if (start) begin
-        ns = READY;
         start_if = 0;
         w_read = 1;
         switch = 0;
@@ -37,7 +36,6 @@ module w_controller (
         ready = 1;
         clr_w = 1'bx;
         ns = READY;
-
       end
     end else if (cs == FETCH) begin
       if (~w_done) begin
@@ -54,7 +52,7 @@ module w_controller (
         ready = 1;
         clr_w = 1'bx;
         ns = READY;
-      end else if (w_done & ~if_ready) begin
+      end else begin
         start_if = 0;
         w_read = 0;
         switch = 0;
@@ -78,11 +76,19 @@ module w_controller (
         clr_w = 1'bx;
         ns = READY;
       end
+    end else begin
+      start_if = 'x;
+      w_read = 'x;
+      switch = 'x;
+      ready = 'x;
+      clr_w = 'x;
+      ns = READY;
     end
+
   end
   //switch and if_start are same
   always_ff @(posedge clk) begin
-    if (rst) cs <= 0;
+    if (rst) cs <= READY;
     else cs <= ns;
   end
 
